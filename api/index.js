0b7675d70cde4bae53dc00a1795a1f42f58555cf
tanujmm <mtanuj121@gmail.com>
2025-07-07 19:49:03 +0530

    render7 commit

--- api/index.js
+++ api/index.js
@@ -30,7 +30,10 @@ app.use("/api/listing", listingRouter);
 
 app.use(express.static(path.join(__dirname, "/client/dist")));
 
-app.get("*", (req, res) => {
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+// });
+app.get(/^\/(?!api).*/, (req, res) => {
   res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
 });
 
