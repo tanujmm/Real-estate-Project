7c0bbb80e5db34988fb6c31732368708f17876ab
tanujmm <mtanuj121@gmail.com>
2025-07-07 16:28:11 +0530

    render2 commit

--- client/src/components/ListingItem.jsx
+++ client/src/components/ListingItem.jsx
@@ -2,6 +2,7 @@ import { Link } from "react-router-dom";
 import { MdLocationOn } from "react-icons/md";
 
 export default function ListingItem({ listing }) {
+  if (!listing || !listing._id) return null;
   return (
     <div className="bg-white shadow-md hover:shadow-2xl transition-shadow overflow-hidden rounded-lg w-full sm:w-[330px]">
       <Link to={`/listing/${listing._id}`}>
