045085072146ba0bb940997a0322cfbc8458f5a0
tanujmm <mtanuj121@gmail.com>
2025-07-07 15:45:25 +0530

    render commit

--- api/index.js
+++ api/index.js
@@ -6,6 +6,7 @@ import authRouter from "./routes/auth.route.js";
 import cookieParser from "cookie-parser";
 import listingRouter from "./routes/listing.route.js";
 import cors from "cors";
+import path from "path";
 const app = express();
 
 dotenv.config();
@@ -18,12 +19,21 @@ mongoose
     console.log("Something went wrong" + err);
   });
 
+const __dirname = path.resolve();
+
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors());
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/listing", listingRouter);
+
+app.use(express.static(path.join(__dirname, "/client/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
+
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || "Internal Server Error";
